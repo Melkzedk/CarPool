59a5108de894cc39222bb992ea0183c442ba6d96
unknown <melkywafula29@gmail.com>
2025-08-10 20:59:49 +0300

    more changes

---
+++ frontend/src/pages/RideRequests.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function RideRequests() {
+  return (
+    <div>
+      <h2>Ride Requests</h2>
+      <p>List of people requesting rides will appear here.</p>
+    </div>
+  );
+}
