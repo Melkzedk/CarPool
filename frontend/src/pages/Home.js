5a53affbe970414557b6bb2f2ce57632267b36d0
melkzedek wafula <melkywafula29@gmail.comm>
2025-10-17 08:59:48 +0300

    changes

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -53,7 +53,7 @@ export default function Home() {
                     to={`/join/${event._id}`}
                     className="btn btn-primary btn-sm"
                   >
-                    Join Events
+                    Join Event
                   </Link>
                 </div>
               </div>
