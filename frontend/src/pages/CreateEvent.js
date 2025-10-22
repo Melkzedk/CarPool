fcc4f0deffbde423b540ca29a50aebd171405dc5
Melkzedk <melkywafula29@gmail.com>
2025-10-22 09:55:41 +0300

    small changes

--- frontend/src/pages/CreateEvent.js
+++ frontend/src/pages/CreateEvent.js
@@ -1,4 +1,4 @@
-//pages/CreateEvent.js module
+//pages/CreateEvent.js
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
