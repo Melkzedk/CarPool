10bcaf25a2b5bb9e7f074cec39fa2dd4aba4de2f
Melkzedk <melkywafula29@gmail.com>
2025-10-25 10:40:35 +0300

    small changes

--- backend/routes/events.js
+++ backend/routes/events.js
@@ -19,6 +19,7 @@ router.post("/", authMiddleware, async (req, res) => {
       description,
       seatsAvailable,
       estimatedCost,
+      locationCoordinates,
     } = req.body;
 
     if (!eventName || !eventDate || !time || !location) {
