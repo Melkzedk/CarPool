1e831bd0e982c1972ef6cf6389dd5e60b5526a48
Melkzedk <melkywafula29@gmail.com>
2025-10-26 19:26:14 +0300

    changes

--- frontend/src/pages/CreateEvent.js
+++ frontend/src/pages/CreateEvent.js
@@ -115,7 +115,7 @@ export default function CreateEvent() {
           />
         )}
 
-        <button className="btn btn-custom w-100">Create</button>
+        <button className="btn btn-custom w-100">Create Event</button>
       </form>
     </div>
   );
