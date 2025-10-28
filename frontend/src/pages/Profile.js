6ddf47513fbd97145acfcf0f4cd5f9d4f2ab2097
Melkzedk <melkywafula29@gmail.com>
2025-10-28 09:09:36 +0300

    small changes

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -27,7 +27,7 @@ export default function Profile() {
       <p><strong>Email:</strong> {user.email}</p>
       <p><strong>Phone:</strong> {user.phoneNumber}</p>
       <p><strong>Role:</strong> {user.role}</p>
-      <p><strong>Designation</strong>{user.Designation}</p>
+      // <p><strong>Designation</strong>{user.Designation}</p>
 
       {/* Show driver details only if role is driver */}
       {user.role === "driver" && (
