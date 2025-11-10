2b41c53a4cca9fddc6c96d21add7c6156885b518
Melkzedk <melkywafula29@gmail.com>
2025-11-10 22:37:33 +0300

    changes

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -36,7 +36,7 @@ export default function Profile() {
           <h5 className="mb-2">Driver Details</h5>
           <p><strong>Car Model:</strong> {user.carModel}</p>
           <p><strong>License Plate!:</strong> {user.licensePlate}</p>
-          <p><strong>Driving License:</strong> {user.drivingLicenseNumber}</p>
+          <p><strong>Driving License!:</strong> {user.drivingLicenseNumber}</p>
         </>
       )}
     </div>
