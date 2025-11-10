28ede0fcbef7f47a46b6e9040a56a995d34f5b4b
Melkzedk <melkywafula29@gmail.com>
2025-11-10 23:01:12 +0300

    changes

--- frontend/src/test.js
+++ frontend/src/test.js
@@ -50,3 +50,7 @@
     console.log("Test");
     console.log("Test Test Test");
 
+     //You can add test cases here to verify functionality
+    console.log("Test Test");
+    console.log("Test");
+    console.log("Test Test Test");
