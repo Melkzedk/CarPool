2d251f234018e1c9666d605f734dd5a0c001a1a7
Melkzedk <melkywafula29@gmail.com>
2025-11-13 08:41:22 +0300

    small changes

--- frontend/src/test.js
+++ frontend/src/test.js
@@ -50,7 +50,6 @@
     console.log("Test");
     console.log("Test Test Test");
 
-     //You can add test cases here to verify functionality
     console.log("Test Test");
     console.log("Test");
     console.log("Test Test Test");
