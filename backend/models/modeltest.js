626aeb5e42e836d1c7559fd191106e9c37901b61
Melkzedk <melkywafula29@gmail.com>
2025-11-13 20:51:06 +0300

    chnages

--- backend/models/modeltest.js
+++ backend/models/modeltest.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const ModelTestSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
-    age: { type: Number, required: true }
+    age: { type: Number, required: true },
+    lastname: { type: String, required: true }
   },
   {
     timestamps: true
