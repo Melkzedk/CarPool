1c0873bddbe70529769b2b4d1087e9fc8eb20231
Melkzedk <melkywafula29@gmail.com>
2025-11-13 20:53:39 +0300

    chnages

--- backend/models/modeltest.js
+++ backend/models/modeltest.js
@@ -5,8 +5,8 @@ const ModelTestSchema = new mongoose.Schema(
     fullname: { type: String, required: true },
     lastname: { type: String, required: true },
     isActive: { type: Boolean, default: true },
-    
     age: { type: Number, required: true },
+    email: { type: String, required: true, unique: true }
   },
   {
     timestamps: true
