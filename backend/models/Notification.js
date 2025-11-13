403cc56f2194dcff34fef64887339e2d0cded338
Melkzedk <melkywafula29@gmail.com>
2025-11-13 10:00:57 +0300

    added timestamp

--- backend/models/Notification.js
+++ backend/models/Notification.js
@@ -8,6 +8,7 @@ const NotificationSchema = new mongoose.Schema(
     event: { type: mongoose.Schema.Types.ObjectId, ref: "Event" },
     message: { type: String, required: true },
     read: { type: Boolean, default: false },
+    Age: {type: Number},
     timestamps: { type: Date, default: Date.now },
   },
   { timestamps: true }
