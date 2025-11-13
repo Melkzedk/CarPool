a2614a595f7783938c43c8e17e23fbe9b7a384f9
Melkzedk <melkywafula29@gmail.com>
2025-11-13 09:46:43 +0300

    changes

--- frontend/src/test.js
+++ frontend/src/test.js
@@ -11,6 +11,7 @@
     // You can add test cases here to verify functionality.
 
     console.log("Make sure to run this file to check for any errors or issues.");
+    console.log("End of test file.");
     // Add more test cases as needed.
 
     console.log("End of test file.");
