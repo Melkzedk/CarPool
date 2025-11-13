6c342c650147cf8ffd308a8ec6bd33c4163b41e8
Melkzedk <melkywafula29@gmail.com>
2025-11-13 08:41:03 +0300

    small changes

--- frontend/src/test.js
+++ frontend/src/test.js
@@ -5,6 +5,8 @@
 
     console.log("Make sure to run this file to check for any errors or issues.");
 
+    console.log("End of test file.");
+
     console.log("Testing complete.");
     // You can add test cases here to verify functionality.
 
@@ -51,3 +53,4 @@
      //You can add test cases here to verify functionality
     console.log("Test Test");
     console.log("Test");
+    console.log("Test Test Test");
