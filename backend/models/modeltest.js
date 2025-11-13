669f6855b957c0c32904440c0b8b85d47b8fddde
Melkzedk <melkywafula29@gmail.com>
2025-11-13 10:26:23 +0300

    small changes

---
+++ backend/models/modeltest.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const ModelTestSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    age: { type: Number, required: true }
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = mongoose.model("ModelTest", ModelTestSchema);
