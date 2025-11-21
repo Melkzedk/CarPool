becde286b4511a6a98772adb477fcddd5048d41b
melkzedek wafula <melkywafula29@gmail.com>
2025-11-21 22:37:55 +0300

    small changes

--- backend/models/modeltest.js
+++ backend/models/modeltest.js
@@ -7,7 +7,6 @@ const ModelTestSchema = new mongoose.Schema(
     fullname: { type: String, required: true },
     lastname: { type: String, required: true },
     emailVerified: { type: Boolean, default: false },
-    Printer: {type: String },
     isActive: { type: Boolean, default: true },
     age: { type: Number, required: true },
     email: { type: String, required: true, unique: true },
